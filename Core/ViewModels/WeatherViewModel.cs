3fed876299d3941f2223b50570f1c9fc2d9e0a3c
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2019-01-30 09:25:00 +0100

    #1 Add MVVMCross

---
+++ Core/ViewModels/WeatherViewModel.cs
@@ -0,0 +1,23 @@
+﻿using API;
+using MvvmCross.ViewModels;
+
+namespace Core.ViewModels
+{
+    public class WeatherViewModel : MvxViewModel
+    {
+        private readonly IApiClient apiClient;
+
+        public WeatherViewModel(IApiClient apiClient)
+        {
+            this.apiClient = apiClient;
+        }
+
+        private string cityName;
+
+        public string CityName
+        {
+            get => cityName;
+            set => SetProperty(ref cityName, value);
+        }
+    }
+}
