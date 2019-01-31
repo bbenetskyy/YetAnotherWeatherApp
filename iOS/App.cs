aaac9591cd5a9308cf5888e8274a528a5151f9b2
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2019-01-31 10:39:49 +0100

    #6 update ios project structure

---
+++ iOS/App.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace iOS
+{
+    public class App
+    {
+        public static bool UseMockDataStore = true;
+        public static string BackendUrl = "http://localhost:5000";
+
+        public static void Initialize()
+        {
+            if (UseMockDataStore)
+                ServiceLocator.Instance.Register<IDataStore<Item>, MockDataStore>();
+            else
+                ServiceLocator.Instance.Register<IDataStore<Item>, CloudDataStore>();
+        }
+    }
+}
