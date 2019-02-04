20739cc4fb44c8be6c7af603af2dafc405dea3be
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2019-02-04 13:54:58 +0100

    #7 Fix Code Factor Issues

--- Android/SplashScreen.cs
+++ Android/SplashScreen.cs
@@ -3,7 +3,6 @@ using MvvmCross.Droid.Support.V7.AppCompat;
 
 namespace Android
 {
-
     [Activity(Theme = "@style/SplashScreen", MainLauncher = true, NoHistory = true)]
     public class SplashScreen : MvxSplashScreenAppCompatActivity
     {
